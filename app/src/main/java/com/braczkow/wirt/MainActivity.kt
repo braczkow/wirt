b1fc6d740b268bd7e2e622a3fb7e2b350cd2ec8f
Bartosz Raczkowski <bartosz.raczkowski@meelogic.com>
2020-08-05 14:59:17 +0200

    Add fragNav, add LocationFragment UI

--- app/src/main/java/com/braczkow/wirt/MainActivity.kt
+++ app/src/main/java/com/braczkow/wirt/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
 import com.braczkow.wirt.openweather.internal.OpenWeatherApi
 import com.braczkow.wirt.openweather.WillItRainUseCase
+import com.ncapdevi.fragnav.FragNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,19 +15,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val api = OpenWeatherApi.Builder(
-            appId
-        ).build()
-        val uc = WillItRainUseCase(api)
+        val fragNavController = FragNavController(supportFragmentManager, R.id.fragment_container)
 
-        val szczecinDescriptor = WillItRainUseCase.LocationDescriptor.LatLon("53.42894", "14.55302")
-        val katowiceDescriptor = WillItRainUseCase.LocationDescriptor.LatLon("50.25841", "19.02754")
+        val fragments = listOf(
+            LocationFragment()
+        )
 
-        lifecycleScope.launchWhenStarted {
-            val willItRain = uc.execute(katowiceDescriptor, WillItRainUseCase.TimeRangeDescriptor.Today)
+        fragNavController.rootFragments = fragments
+        fragNavController.initialize(FragNavController.TAB1, savedInstanceState)
 
-            main_text.text = "Will it rain: $willItRain"
-        }
+
+//        val api = OpenWeatherApi.Builder(
+//            appId
+//        ).build()
+//        val uc = WillItRainUseCase(api)
+//
+//        val szczecinDescriptor = WillItRainUseCase.LocationDescriptor.LatLon("53.42894", "14.55302")
+//        val katowiceDescriptor = WillItRainUseCase.LocationDescriptor.LatLon("50.25841", "19.02754")
+//
+//        lifecycleScope.launchWhenStarted {
+//            val willItRain = uc.execute(katowiceDescriptor, WillItRainUseCase.TimeRangeDescriptor.Today)
+//        }
 
     }
 }
