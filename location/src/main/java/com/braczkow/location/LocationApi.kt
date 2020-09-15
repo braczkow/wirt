74a8b359020f3f54ec3045e6be31ddfbda488ab8
Bartosz Raczkowski <bartosz.raczkowski@meelogic.com>
2020-09-15 10:36:06 +0200

    LocationApi upgraded

--- location/src/main/java/com/braczkow/location/LocationApi.kt
+++ location/src/main/java/com/braczkow/location/LocationApi.kt
@@ -1,13 +1,27 @@
 package com.braczkow.location
 
+import android.Manifest
 import android.content.Context
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import android.content.pm.PackageManager
+import android.os.Looper
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.braczkow.lib.Logg
+import com.google.android.gms.location.*
 
 interface LocationApi {
-    class LocationRequest
+    interface HighAccuracyRequest {
+        fun start()
+        fun stop()
+    }
 
+    data class Location(
+        val lat: Float,
+        val long: Float,
+        val accuracy: Float
+    )
 
+    fun makeLocationRequest(): HighAccuracyRequest
 }
 
 class LocationApiImpl(
@@ -15,8 +29,52 @@ class LocationApiImpl(
 ) : LocationApi {
 
     private val client: FusedLocationProviderClient
+    private val locationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult?) {
+
+        }
+    }
+
+    private val requests = mutableSetOf<LocationRequest>()
 
     init {
         client = LocationServices.getFusedLocationProviderClient(context)
+    }
+
+    override fun makeLocationRequest(): LocationApi.HighAccuracyRequest {
+
+        val locationRequest = LocationRequest
+            .create()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+
+        return object : LocationApi.HighAccuracyRequest {
+            override fun start() {
+                if (ContextCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    )
+                    != PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    Logg.d("No location permission, skip locationRequest!")
+                    return
+                }
+
+                requests.add(locationRequest)
+                client.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+            }
+
+            override fun stop() {
+                requests.remove(locationRequest)
+
+                if (requests.isEmpty()) {
+                    client.removeLocationUpdates(locationCallback)
+                }
+
+            }
+        }
     }
 }
